4c54c7c755afb50230b51d2b1585a94e42e1ac92
Prakritish Sen Eshore <prakritish@endor.ai>
2023-05-23 01:26:49 +0530

    Initial Commit

---
+++ src/action.js
@@ -0,0 +1,84 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const request = require('request');
+
+try {
+    const pass = 'PASS';
+    const fail = 'FAIL';
+    const pass_color = '#009933';
+    const fail_color = '#e63900';
+    const url = 'https://slack.com/api/chat.postMessage';
+    const slackToken = core.getInput('slack_token');
+    const channel = core.getInput('channel');
+    const message = core.getInput('message');
+    const result = core.getInput('result');
+    const fields = core.getInput('fields');
+    if (result.localeCompare(pass, 'en', {sensitivity: 'base'})) {
+        color = pass_color
+    } else if (result.localeCompare(fail, 'en', {sensitivity: 'base'})) {
+        color = fail_color
+    }
+    data = {
+        channel: channel,
+        attachments: [
+            {
+                blocks: [
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: message
+                        }
+                    },
+                    {
+                        type: "section",
+                        fields: []
+                    }
+                ]
+            }
+        ]
+    }
+    if (result) {
+        data['attachments'][0]['color'] = color
+    }
+    const fieldList = fields.split(/\r?\n/).filter(Boolean);
+    if (fieldList.length) {
+        fieldList.forEach(addFields);
+    } else {
+        delete data['attachments'][0]['blocks'][1];
+    }
+    
+    const options = {
+        url: url,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        auth: {
+            bearer: slackToken
+        },
+        json: data
+    };
+    request.post(options, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body)
+        }
+        else {
+            console.log("error: " + error)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusText)
+            core.setFailed(error)
+        }
+    })
+} catch (error) {
+    core.setFailed(error.message);
+}
+
+function addFields(line) {
+    const field = line.split(/:(.*)/).filter(Boolean)
+    const fieldValue = {
+        type: "mrkdwn",
+        text: "*" + field[0] + "*\n" + field[1]
+    }
+    data['attachments'][0]['blocks'][1]['fields'].push(fieldValue)
+}
